ad2ed440271e4dd092f425ed3bd54e9ee1efddee
Showk Mahmoud <showk.mahmoud@plark.com>
2021-09-24 16:24:05 +0200

    footer, error layout and home page done

--- src/app/layouts/landing/components/landing-layout/landing-layout.component.ts
+++ src/app/layouts/landing/components/landing-layout/landing-layout.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing-layout',
   templateUrl: './landing-layout.component.html',
-  styleUrls: ['./landing-layout.component.scss']
+  styleUrls: ['./landing-layout.component.scss'],
 })
 export class LandingLayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  route!: string;
+  constructor(location: Location, router: Router) {
+    router.events.subscribe((val) => {
+      if (location.path() != '') {
+        this.route = location.path();
+      } else {
+        this.route = 'Home';
+      }
+    });
   }
 
+  ngOnInit(): void {}
 }
