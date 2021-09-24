dbcaa8c142b548286919bb5e5d2580facc2b31bc
Showk Mahmoud <showk.mahmoud@plark.com>
2021-09-24 15:22:16 +0200

    header done

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  userName: string = 'sabry elsayed farag mahmoud';
+  menuOpen = false;
+  route!: string;
+  constructor(location: Location, router: Router) {
+    router.events.subscribe((val) => {
+      if (location.path() != '') {
+        this.route = location.path();
+      } else {
+        this.route = 'Home';
+      }
+    });
   }
+  ngOnInit(): void {}
 
+  onToggle() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
--- src/app/layouts/landing/landing-routing.module.ts
+++ src/app/layouts/landing/landing-routing.module.ts
@@ -5,18 +5,19 @@ import { ProductPageComponent } from 'src/app/pages/components/product-page/prod
 import { LandingLayoutComponent } from './components/landing-layout/landing-layout.component';
 
 const routes: Routes = [
-  {path:'',
-  component:LandingLayoutComponent,
-  children:[
-    {path: '', redirectTo:'home',pathMatch:'full'},
-    {path: 'home', component:HomePageComponent},
-    {path:'product', component:ProductPageComponent}
-  ]
-},
+  {
+    path: '',
+    component: LandingLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomePageComponent },
+      { path: 'product', component: ProductPageComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LandingRoutingModule { }
+export class LandingRoutingModule {}
